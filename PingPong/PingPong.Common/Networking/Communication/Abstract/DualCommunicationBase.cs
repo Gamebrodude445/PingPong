a40a1b70828dac963f6428687c8d082f4e934206
gamer <fantadrinkepicemail@gmail.com>
2022-01-19 16:07:51 +0200

    New File: Add Skeleton for TCP

--- PingPong/PingPong.Common/Networking/Communication/Abstract/DualCommunicationBase.cs
+++ PingPong/PingPong.Common/Networking/Communication/Abstract/DualCommunicationBase.cs
@@ -1,6 +1,6 @@
 ﻿namespace PingPong.Common.Networking.Communication.Abstract
 {
-    public abstract class SocketBase<T> : IReciever<T>, ISender<T>
+    public abstract class DualCommunicationBase<T> : IReciever<T>, ISender<T>
     {
         public abstract T Receive();
         public abstract bool TrySend(T toSend);
