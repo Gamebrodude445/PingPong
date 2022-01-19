11fa0021b45b86e8affdc6e741e3ac6b22d3a37b
gamer <fantadrinkepicemail@gmail.com>
2022-01-19 17:17:38 +0200

    Feature Work: Start Basic Work on Simple Work

--- Server/PingPong.Server/PingPong.Server/Program.cs
+++ Server/PingPong.Server/PingPong.Server/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace PingPong.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            socket.Listen();
+            Thread newThread = new Thread(() =>
+                {
+                    var socket1 = socket;
+                    var newSocket = socket1.Accept();
+                }
+            );
         }
     }
 }
