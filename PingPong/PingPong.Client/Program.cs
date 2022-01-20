05189cd8ab6de2ad273d19ca19353bf275ff3f65
gamer <fantadrinkepicemail@gmail.com>
2022-01-20 10:37:16 +0200

    Feature Work: Change Access Modifiers

--- PingPong/PingPong.Client/Program.cs
+++ PingPong/PingPong.Client/Program.cs
@@ -2,7 +2,7 @@
 
 namespace PingPong.Client
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- PingPong/PingPong.Server/Program.cs
+++ PingPong/PingPong.Server/Program.cs
@@ -2,7 +2,7 @@
 
 namespace PingPong.Server
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
