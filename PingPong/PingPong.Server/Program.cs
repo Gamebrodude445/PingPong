2c40ce7b21c6f871384042c221828e7b85866e1d
gamer <fantadrinkepicemail@gmail.com>
2022-01-20 10:34:57 +0200

    New File: Add VS files

---
+++ PingPong/PingPong.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PingPong.Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
