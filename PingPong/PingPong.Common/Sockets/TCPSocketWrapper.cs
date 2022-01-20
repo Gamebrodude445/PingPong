fdf4f619e6b1a88e2d73a809a3e06a0b7df9e219
gamer <fantadrinkepicemail@gmail.com>
2022-01-20 12:06:39 +0200

    Feature Work: Add Props

--- PingPong/PingPong.Common/Sockets/TCPSocketWrapper.cs
+++ PingPong/PingPong.Common/Sockets/TCPSocketWrapper.cs
@@ -1,9 +1,20 @@
 ﻿using PingPong.Common.Sockets.Abstractions;
+using System.Net.Sockets;
 
 namespace PingPong.Common.Sockets
 {
     public class TCPSocketWrapper<T> : ISocketWrapper<T>
     {
+        private Socket TCPSocket { get; set; }
+        private string _ip { get; set; }
+        private uint _port { get; set; }
+
+        public TCPSocketWrapper(string ip, uint port)
+        {
+            TCPSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            _ip = ip;
+            _port = port;
+        }
         public void Connect()
         {
             throw new NotImplementedException();
